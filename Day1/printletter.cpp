8cc02b08d2789c63f78acdb62f1706c31fc7d190
yashasvi-labs <143244861+yashasvisharma20@users.noreply.github.com>
2026-01-30 19:32:27 +0530

    day 1 add new file

---
+++ Day1/printletter.cpp
@@ -0,0 +1,9 @@
+#include <iostream> // preprocessor directive to use input output stream from standar library
+using namespace std; // to avoid using std:: again and again
+
+int main(){
+  char letter; // variable define to store value of letter
+  cin >> letter;
+  cout << "Letter: " << letter << endl;
+  return 0;
+}
