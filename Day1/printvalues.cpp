3e530b47bd75649bbe838682b47621b800e9cbdb
yashasvi-labs <143244861+yashasvisharma20@users.noreply.github.com>
2026-01-30 19:38:38 +0530

    Implement main function to print user input values

---
+++ Day1/printvalues.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  // define integer variables
+  int var1, var2, var3;
+  // take input from the user and read variables
+  cin >> var1 >> var2 >> var3;
+  // print all variables
+  cout << "You entered: " << var1 << "," << var2 << "," << var3 << endl;
+  return 0;
+}
